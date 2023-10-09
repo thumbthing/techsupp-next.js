d0e6f2f003e393df12f262a5783aee3163c1a053
thumbthing <ilscc0902@gmail.com>
2023-10-10 04:00:39 +0900

    Feat: 페이지 스타일  초기화, 메인 페이지 기초 라우팅 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Home() {
+  const path = {
+    product: 'product',
+    feedback: 'feedback',
+  };
+
   return (
     <main>
       <div>techsupp next js version</div>
+      <div>
+        <Link href={path.product}>
+          <button type="button">product</button>
+        </Link>
+        <Link href={path.feedback}>
+          <button type="button">feedback</button>
+        </Link>
+      </div>
     </main>
   );
 }
