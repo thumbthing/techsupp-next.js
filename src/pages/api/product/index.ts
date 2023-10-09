d6bd9144597c3bb7ac0760d3859783c1297a6f15
thumbthing <ilscc0902@gmail.com>
2023-10-10 04:10:21 +0900

    Feat: MongoDB로 부터 product 데이터 관리하는 handler 생성(GET 기능 구현)

---
+++ src/pages/api/product/index.ts
@@ -0,0 +1,19 @@
+import dbConnect from '@/lib/mongoose/dbConnect';
+import Product from '@/lib/product/product.model';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      await dbConnect();
+      const data = await Product.find({});
+      console.log('data', data);
+      console.log('json', res.status(200).json(data));
+
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error('DB handler error occurred', error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+}
