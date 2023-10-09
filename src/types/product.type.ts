d77bea3ff2cbd70755b5ff83bdeec011476a958d
thumbthing <ilscc0902@gmail.com>
2023-10-10 08:22:39 +0900

    Feat: product 타입 선언

---
+++ src/types/product.type.ts
@@ -0,0 +1,12 @@
+interface ProductType {
+  _id: string;
+  name: string;
+  category: string;
+  total_invest_price: number;
+  date: string;
+  information: string;
+  invest_price: number;
+  isInvesting: boolean;
+}
+
+export default ProductType;
