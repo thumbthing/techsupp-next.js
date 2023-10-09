5bd61c81a1cbca2d039e0b4fbe8b1ee143ca5360
thumbthing <ilscc0902@gmail.com>
2023-10-10 04:07:28 +0900

    Refactor: global 선언을 타입으로 변경, 디렉터리 위치 변경

--- globals.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  namespace NodeJS {
-    interface Global {
-      mongoose: typeof import('mongoose');
-    }
-  }
-}
---
+++ src/types/mongodb.ts
@@ -0,0 +1,9 @@
+import { Mongoose } from 'mongoose';
+
+/* eslint-disable no-var */
+declare global {
+  var mongoose: {
+    promise: Promise<Mongoose> | null;
+    conn: Mongoose | null;
+  };
+}
