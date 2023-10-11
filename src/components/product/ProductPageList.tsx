5b499a542030a01f4a91608c947088ed30c5745e
thumbthing <ilscc0902@gmail.com>
2023-10-12 02:13:16 +0900

    Feat: 페이지네이션 기능 담당 컴포넌트 생성

---
+++ src/components/product/ProductPageList.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { RootState } from '@/store/productStore';
+import { setPageIndex, setPageList, setPageScope } from '@/store/slices/productSlice';
+import ProductType from '@/types/product.type';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import '../../style/product/productPage.style.css';
+
+function ProductPageList() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const filteredList = useSelector((state: RootState) => state.product.filteredProductList);
+  const pageList = useSelector((state: RootState) => state.product.pageList);
+  const pageIndex = useSelector((state: RootState) => state.product.pageIndex);
+  const pageScope = useSelector((state: RootState) => state.product.pageScope);
+
+  const prevScope = pageScope - 1 >= 0 ? pageScope - 1 : 0;
+  const nextScope = pageScope + 1 < Math.ceil(filteredList.length / 50) ? pageScope + 1 : pageScope;
+
+  useEffect(() => {
+    const getPages = (productList: ProductType[]) => {
+      const maxPageIndex = Math.ceil(productList.length / 5);
+      const pages = [];
+      for (let i = 0; i < maxPageIndex; i++) {
+        pages.push(i);
+      }
+      return pages;
+    };
+
+    const filteredPageList = getPages(filteredList);
+    dispatch(setPageList(filteredPageList));
+    dispatch(setPageScope(Number(router.query.scope)));
+  }, [filteredList, dispatch, router]);
+
+  const changePage = (pageNumber: number) => {
+    dispatch(setPageIndex(pageNumber));
+  };
+
+  const decreaseScope = () => {
+    router.push(`/product?scope=${prevScope}`);
+  };
+
+  const increaseScope = () => {
+    router.push(`/product?scope=${nextScope}`);
+  };
+
+  return (
+    <article>
+      <ul className="page-list">
+        <li key={`page-previous`} className="page-button">
+          <button onClick={decreaseScope}>이전</button>
+        </li>
+        {pageList
+          .filter((item, index) => index >= pageScope * 10 && index < pageScope * 10 + 10)
+          .map((item) => (
+            <li key={`page=${item}`} className="page-button">
+              <button onClick={() => changePage(item)}>{item + 1}</button>
+            </li>
+          ))}
+        <li key={`page-next`} className="page-button">
+          <button onClick={increaseScope}>이후</button>
+        </li>
+      </ul>
+    </article>
+  );
+}
+
+export default ProductPageList;
