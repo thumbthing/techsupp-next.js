8a75ac5a74549e2f86cf69197fba6df0c29c8a7a
thumbthing <ilscc0902@gmail.com>
2023-10-13 06:56:19 +0900

    Refactor: Product 리스트 정렬 기능 가독성, 관심사 분리, 중복 코드 최소화

---
+++ src/util/product/getRetrievedList.ts
@@ -0,0 +1,95 @@
+import ProductType from '@/types/product.type';
+
+function getRetrievedList(
+  primaryList: ProductType[],
+  modifiedList: ProductType[],
+  searchText: string,
+  orderDirection: string,
+  isChecked: boolean,
+  keyword: 'name' | 'price' | 'date' | '',
+) {
+  const selectList = (isChecked: boolean, text: string, primaryList: ProductType[], modifiedList: ProductType[]) => {
+    if (isChecked) {
+      const searchedList = [...modifiedList].filter((product) => product.name.includes(text));
+      return searchedList;
+    } else {
+      const searchInRetrievedList = [...primaryList].filter((product) => product.name.includes(text));
+      return searchInRetrievedList;
+    }
+  };
+
+  const sortByName = (list: ProductType[], orderDirection: String) => {
+    const result = [...list].sort((a, b) =>
+      orderDirection === 'ASC' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name),
+    );
+    return result;
+  };
+
+  const sortByPrice = (list: ProductType[], orderDirection: String) => {
+    const result = [...list].sort((a, b) =>
+      orderDirection === 'ASC' ? a.invest_price - b.invest_price : b.invest_price - a.invest_price,
+    );
+    return result;
+  };
+
+  const sortByDate = (list: ProductType[], orderDirection: String) => {
+    const result = [...list].sort((a, b) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+      if (orderDirection === 'ASC') {
+        return dateA - dateB;
+      } else {
+        return dateB - dateA;
+      }
+    });
+    return result;
+  };
+
+  const sortById = (list: ProductType[], orderDirection: String) => {
+    const result = [...list].sort((a, b) =>
+      orderDirection === 'ASC' ? a._id.localeCompare(b._id) : b._id.localeCompare(a._id),
+    );
+    return result;
+  };
+
+  const selectSortingRule = (key: 'name' | 'price' | 'date' | '') => {
+    if (key === 'name') {
+      return sortByName;
+    }
+    if (key === 'price') {
+      return sortByPrice;
+    }
+    if (key === 'date') {
+      return sortByDate;
+    }
+    if (key === '') {
+      return sortById;
+    }
+  };
+
+  const getSearchedList = (
+    isChecked: boolean,
+    text: string,
+    keyword: 'name' | 'price' | 'date' | '',
+    primaryList: ProductType[],
+    modifiedList: ProductType[],
+    orderDirection: string,
+  ) => {
+    const selectedList = selectList(isChecked, text, primaryList, modifiedList);
+
+    const sortRule = selectSortingRule(keyword);
+
+    if (sortRule) {
+      const retrievedList = sortRule(selectedList, orderDirection);
+      return retrievedList;
+    }
+  };
+
+  const result = getSearchedList(isChecked, searchText, keyword, primaryList, modifiedList, orderDirection);
+
+  if (result) {
+    return result;
+  }
+}
+
+export default getRetrievedList;
