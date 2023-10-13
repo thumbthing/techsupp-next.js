69c94843e53065d23ad2cdb8489d22b0b26a7413
thumbthing <ilscc0902@gmail.com>
2023-10-14 05:41:04 +0900

    Refactor: 디렉터리 구조 변경에 의한 파일 위치 변경

---
+++ src/app/product/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import ProductList from '@/components/product/ProductList';
+import React, { useEffect } from 'react';
+import ProductPageList from '@/components/product/ProductPageList';
+import '../../style/product/productPage.style.css';
+import ProductSearchBar from '@/components/product/ProductSearchBar';
+import ProductPageLoading from '@/components/loading/Product.Loading';
+import { useDispatch, useSelector } from 'react-redux';
+import { GlobalState } from '@/redux/store/globalStore';
+import { setLoadingStatus } from '@/redux/slices/loadingSlice';
+
+function ProductPage() {
+  const loadingState = useSelector((state: GlobalState) => state.loading.isLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (loadingState === true) {
+      timer = setTimeout(() => {
+        dispatch(setLoadingStatus(loadingState));
+      }, 3000);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [loadingState, dispatch]);
+
+  return (
+    <>
+      {loadingState ? (
+        <ProductPageLoading />
+      ) : (
+        <main className="main-box">
+          <ProductSearchBar />
+          <ProductList />
+          <ProductPageList />
+        </main>
+      )}
+    </>
+  );
+}
+
+export default ProductPage;
