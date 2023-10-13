e428d516087b3fb9318cd14e281e614722547617
thumbthing <ilscc0902@gmail.com>
2023-10-13 16:16:39 +0900

    Feat: loading provider를 페이지에 적용

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -2,21 +2,23 @@
 
 import ProductList from '@/components/product/ProductList';
 import React from 'react';
-import styled from 'styled-components';
-import ProductProvider from '../../store/ProductProvider';
+import ProductProvider from '../../store/provider/ProductProvider';
 import ProductPageList from '@/components/product/ProductPageList';
 import '../../style/product/productPage.style.css';
 import ProductSearchBar from '@/components/product/ProductSearchBar';
+import LoadingProvider from '@/store/provider/LoadingProvider';
 
 function ProductPage() {
   return (
-    <ProductProvider>
-      <main className="main-box">
-        <ProductSearchBar />
-        <ProductList />
-        <ProductPageList />
-      </main>
-    </ProductProvider>
+    <LoadingProvider>
+      <ProductProvider>
+        <main className="main-box">
+          <ProductSearchBar />
+          <ProductList />
+          <ProductPageList />
+        </main>
+      </ProductProvider>
+    </LoadingProvider>
   );
 }
 
