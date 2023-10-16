07fb2c10914c11311a35bf0291a9206ebd300783
thumbthing <ilscc0902@gmail.com>
2023-10-16 17:29:40 +0900

    Feat: ISO 날짜 형식 파싱 기능 커스텀 hook 생성

---
+++ src/hooks/useDateParser.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
+
+function useDateParser(date: string) {
+  const [parsedDate, setParsedDate] = useState<string>('');
+
+  useEffect(() => {
+    const isoDate = new Date(date);
+    const formatedDate = new Intl.DateTimeFormat('ko-KR', options).format(isoDate);
+    setParsedDate(formatedDate);
+  }, [date]);
+
+  return parsedDate;
+}
+
+export default useDateParser;
